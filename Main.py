a4d0428489736fedcd407b075b6c18df97b43e04
SjsjsjjsuTzms <158812307+SjsjsjjsuTzms@users.noreply.github.com>
2024-11-15 21:29:15 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,108 +1,98 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup , InlineQueryResultArticle, InputTextMessageContent , CallbackQuery
-from pyrogram.types.bots_and_keyboards import force_reply 
-from pyrogram import Client,types,filters
-from pyromod import listen
-import random, string
-from asyncio import sleep
+from selenium.webdriver import Keys, ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+from bs4 import BeautifulSoup
 import time
-import json
-import re
-from soroush import Client as SClient
 
+async def find_element(app, value, by=By.XPATH, timeout=45) -> WebElement:
+    end_time = time.time() + timeout
+    alternate_paths = {
+        "/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/input": 
+            "/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/input",
+        "/html/body/div[2]/div/div/div[2]/div[3]/div[2]/div/div[2]/div[1]/button": 
+            "/html/body/div[2]/div/div/div[2]/div[4]/div[2]/div/div[2]/div[1]/button",
+        "/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/input": 
+            "/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/input",
+        "/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/button[2]": 
+            "/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/button[2]",
+        "/html/body/div[2]/div/div/div[2]/div[3]/div[2]/div/div[2]/div[1]/button": 
+            "/html/body/div[2]/div/div/div[2]/div[4]/div[2]/div[2]/div[2]/div[1]/button"
+    }
 
-
-
-
-database= {'SLEEPTIME': 1, 'banner1' : None, 'banner2' : None, 'name' : None}
-groupText=str(database['banner1'])
-START=InlineKeyboardMarkup([
-[InlineKeyboardButton("✅ اضافه کردن اکانت ✅" , "Addaccount")],
-[ InlineKeyboardButton("⚙ تنظیم بنر یک ⚙" , "banner1")],
-])
-
-cancell=ReplyKeyboardMarkup([['/cancell']],resize_keyboard =True)
-wait=ReplyKeyboardMarkup([['منتظر باش دلقک']],resize_keyboard =True)
-yesno=ReplyKeyboardMarkup([
-['Yes','No'],
-],resize_keyboard =True)
-
-def randomword(length=5):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
-    
-    
-def lichNumber(num):
-    return "\n".join([num + str(random.randint(1000000, 9999999)) for i in range(10000)])
-  
-proxy = None
-Owner=[5394456754] # ایدی عددی بزار
-token="8023919010:AAFtivUU-IGSpfUYmTnEWWrOZ7LsRkxoQKM"
-Bot=Client("CreateBot",api_id=15567484,api_hash="9cee14fbc3ea1fefd4bbb4fd4e2daa6d",bot_token=token)
-
-      
-
-@Bot.on_message(filters.user(Owner) & filters.command("start") )
-async def start(_:Bot,Event:types.Message):
-
-    await Event.reply(f"""
-
-Banner (1) : {database['banner1']}
-
-Channel : @PyUnknown""",reply_markup=START)
-
-
-
-@Bot.on_callback_query(filters.user(Owner) & filters.regex("Addaccount"))
-async def getphone(Event:types.Message,Call:CallbackQuery):
-    phone=await Bot.ask(chat_id=Call.message.chat.id,text="Enter Phone Number or /cancell",reply_markup=cancell)
-    if phone.text=="/cancell":
-        await Call.message.reply("cancell Ok",reply_markup=START)
-    else:       
-        Final = phone.text
-        Final = phone.text.translate(str.maketrans('۰۱۲۳۴۵۶۷۸۹', '0123456789'))        
-        Final = Final.replace("+98", "09")                        
-        app = await SClient(Final)
-        print (app)
-        result=await Bot.ask(chat_id=Call.message.chat.id,text="Enter code or /cancell",reply_markup=cancell)        
-        if result.text=="/cancell":
-            await Call.message.reply("cancell Ok",reply_markup=START)            
-        response = await app.login(result.text)
-        if response:
-            await Call.message.reply(f"Login to {Final} was successful",reply_markup=wait)            
-            num=await Bot.ask(chat_id=Call.message.chat.id,text="لیست شماره هاتو بفرست \n 09128610029 \n09126184872 \n09128097882\n یا /cancell",reply_markup=cancell)
-            if num.text=="/cancell":
-                await Call.message.reply("cancell Ok",reply_markup=START)
-                return
-            for number in num.text.split('\n'):                                
+    while 1:
+        try:
+            return await app.find_element(by, value)
+        except:
+            # Attempt to use an alternative path if one is defined
+            if value in alternate_paths:
+                alt_value = alternate_paths[value]
                 try:
-                    T= await app.check(number)
-                    if T:
-                        await app.send(str(database['banner1']))                
-                except Exception as e:
-                    await Call.message.reply(e)
-            await app.exit()                       
-            await Call.message.reply(f"""❕هشدار:\n- اکانت {Final} با موفقیت از دیتابیس حذف شد ✅""",reply_markup=START)
-        else:
-            if result.text == "/cancell":
-                print ("55")
-            else:
-                await Call.message.reply("ERROR",reply_markup=START)
-                
-            
-           
+                    return await app.find_element(by, alt_value)
+                except:
+                    pass
+            time.sleep(1)
 
-@Bot.on_callback_query(filters.user(Owner) & filters.regex("banner1"))
-async def getpjdjdhohhntse(Event:types.Message,Call:CallbackQuery):
-    time=await Bot.ask(chat_id=Call.message.chat.id,text="""بنرت رو ارسال کن یا برای لغو عملیات /cancell""",reply_markup=cancell)
-    if time.text=="/cancell":
-        await Call.message.reply("cancell Ok",reply_markup=START)
-    else:      
-        database['banner1'] = time.text
-        await Call.message.reply(f"با موفقیت تنظیم شد , \nBanner :\n {time.text}",reply_markup=START)    
-print("Run")
-Bot.run()
+    raise Exception(f"Element not found: {value}")
 
+# تابع برای کلیک بر روی عنصر در Selenium با حداکثر تلاش
+async def click(element):
+    while 1:
+        try:
+            return element.click()
+        except:
+            continue
 
+class Client:
+    
+    async def __init__(self, phone):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        self.app = webdriver.Firefox(options=options)
+        self.app.get("https://web.splus.ir")
+        action = ActionChains(self.app)
+        action.send_keys(phone[1:])
+        action.pause(10)
+        action.send_keys(Keys.ENTER)
+        action.perform()
 
+    async def login(self, code: int):
+        action = ActionChains(self.app)
+        action.send_keys(Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE)
+        action.send_keys(code)
+        action.perform()
+        time.sleep(5)
+        await click(await find_element(self.app, '/html/body/div[2]/div/div/div[1]/div/div[4]/div[4]'))
+    
+    async def check(self, phone):
+        await click(await find_element(self.app, '/html/body/div[2]/div/div/div[1]/div/div[2]/div[2]/div[2]/button'))
+        time.sleep(1)
+        await find_element(self.app, '/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/input').send_keys(phone[1:])
+        time.sleep(0.2)
+        await click(await find_element(self.app, '/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/input'))
+        time.sleep(0.2)
+        await find_element(self.app, '/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/input').send_keys(phone)
+        time.sleep(0.2)
+        await click(await find_element(self.app, '/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/button[2]'))
+        time.sleep(0.2)
+        name = BeautifulSoup(self.app.page_source, "html.parser").find_all("div", {"class":"info"})[-1].find("h3").text
+        if phone == name:
+            return True
+        return False
+    
+    async def send(self, text):
+        time.sleep(0.5)
+        action = ActionChains(self.app)
+        action.send_keys(text)
+        action.pause(0.2)
+        action.send_keys(Keys.ENTER)
+        action.perform()
 
+    async def exit(self):
+        self.app.close()
+        self.app.quit()
 
