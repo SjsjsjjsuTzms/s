a86106b004220a8d10718e11473c23f82f591614
SjsjsjjsuTzms <158812307+SjsjsjjsuTzms@users.noreply.github.com>
2024-11-15 21:45:29 +0330

    Update soroush.py

--- soroush.py
+++ soroush.py
@@ -48,7 +48,7 @@ class Client:
     
     def __init__(self, phone):
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
